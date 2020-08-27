c05a064b48b624066c3be1031c7f1952605031eb
Jamie Barton <jamie@notrab.dev>
2020-08-27 09:02:42 +0100

    add + invoke banner component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,24 @@
 import React from "react";
 
+import Banner from "./Banner";
 import Header from "./Header";
 import Footer from "./Footer";
 
 export default function Layout({ children }) {
   return (
     <React.Fragment>
+      <Banner>
+        We're sponsors of{" "}
+        <a
+          href="https://headlesscommercesummit2020.sched.com/"
+          className="underline"
+          rel="nofollow noopener noreferrer"
+          target="_blank"
+        >
+          Headless Commerce Summit
+        </a>
+        , Sept 3, 2020 &mdash; Come say hello!
+      </Banner>
       <Header />
 
       <main className="bg-athens-gray py-3 md:py-6">{children}</main>
