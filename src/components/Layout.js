7b271ea8e161a4d8c6a826705c56f829fa537274
Jamie Barton <jamie@notrab.dev>
2020-09-04 12:07:08 +0100

    feat: remove headless summit banner (#36)

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,24 +1,10 @@
 import React from "react";
 
-import Banner from "./Banner";
 import Footer from "./Footer";
 
 export default function Layout({ children }) {
   return (
     <React.Fragment>
-      <Banner>
-        We're sponsors of{" "}
-        <a
-          href="https://ti.to/netlify/headless-commerce-summit?source=commercejs"
-          className="underline"
-          rel="nofollow noopener noreferrer"
-          target="_blank"
-        >
-          Headless Commerce Summit
-        </a>
-        , Sept 3, 2020 &mdash; Come say hello!
-      </Banner>
-
       <main className="bg-athens-gray pb-3 md:pb-6">{children}</main>
 
       <Footer />
