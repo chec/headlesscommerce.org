34be09ba1f6fb31a33cf67b2ad20127a0279ce3b
Jamie Barton <jamie@notrab.dev>
2020-10-05 14:29:16 +0100

    fix: extend ActiveLink type

--- src/components/active-link.tsx
+++ src/components/active-link.tsx
@@ -1,12 +1,20 @@
 import React, { Children } from "react";
 import { useRouter } from "next/router";
-import Link from "next/link";
+import Link, { LinkProps } from "next/link";
 
-const ActiveLink = ({ children, activeClassName, ...props }) => {
+type ActiveLinksProps = LinkProps & {
+  activeClassName: string;
+  children: any;
+};
+
+const ActiveLink = ({
+  children,
+  activeClassName,
+  ...props
+}: ActiveLinksProps) => {
   const { asPath } = useRouter();
   const child = Children.only(children);
-    const childClassName = child.props.className || "";
-  const isActive = asPath === props.href || asPath === props.as;
+  const childClassName = child.props.className || "";
 
   const className =
     asPath === props.href || asPath === props.as
