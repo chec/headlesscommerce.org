f09aa8c354dd26c0cf45bfe4ef8e1f8c8f2eacfd
Jamie Barton <jamie@notrab.dev>
2020-10-19 21:01:54 +0100

    add ga

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { DefaultSeo } from "next-seo";
+import Router from "next/router";
 
 import SEO from "../next-seo.config";
 import { LayoutProvider } from "../context/layout";
 import { BookmarksProvider } from "../context/bookmarks";
+import * as gtag from "../lib/ga";
 
 import "../styles/index.css";
+
+Router.events.on("routeChangeComplete", (url) => gtag.pageview(url));
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
